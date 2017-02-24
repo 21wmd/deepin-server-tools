addc87dd86af558a7c58ae7b9d19b4fc3a19ef7f
deepin <deepin@server.21wmd.com>
2017-02-24 18:50:25 +0800

    完善iso生成脚本

---
+++ etc/init.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+init () {
+    directory=$(cd .. ; pwd)
+
+    mkdir -p $directory/archive/{iso,rpm,src,dvdiso}
+
+    isodir=$directory/archive/iso/
+    rpmdir=$directory/archive/rpm/
+    srcdir=$directory/archive/src/
+    dvdiso=$directory/archive/dvdiso/
+
+    isodate=$(date +%Y_%m_%d_%R)
+}
+
+isoinit () {
+    sudo yum install anaconda repodata createrepo mkisofs rsync
+}
+
+rpminit () {
+    sudo yum install rpmdevtools mock
+}

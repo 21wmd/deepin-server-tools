2918c8fff1f3fee2cbfabc2507dca4acbc7be44c
21wmd <wangmd21@gmail.com>
2017-04-14 19:03:12 +0800

    Update mksrc.sh
    update

--- tools/src/mksrc.sh
+++ tools/src/mksrc.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-for i in {1..2171}
+mksrc ()
+{
 
-do
+    for i in {1..2171}
 
-    line=$i'p'
-    name=$(sed -n "$line" ~/list) 
-    rpm -i /mnt/centos/SPackages/$name
-    specs=$(ls ~/rpmbuild/SPECS/)
-    mock -r deepin-15-x86_64 --buildsrpm --spec ~/SPECS/$specs  --sources ~/rpmbuild/SOURCES --resultdir=~/SPackages
-    rm -rf ~/rpmbuild
+    do
 
-done
+        line=$i'p'
+        name=$(sed -n "$line" ~/list) 
+        rpm -i /mnt/centos/SPackages/$name
+        specs=$(ls ~/rpmbuild/SPECS/)
+        mock -r deepin-15-x86_64 --buildsrpm --spec ~/SPECS/$specs  --sources ~/rpmbuild/SOURCES --resultdir=~/SPackages
+        rm -rf ~/rpmbuild
+
+    done
+}
